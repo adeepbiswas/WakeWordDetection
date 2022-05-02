598550f6523c5814946c2bb3246c50d785f0ae1f
danielhhoskins <danielhhoskins@gmail.com>
2022-05-02 16:02:21 -0400

    Adding mish activation to TransformerBlock class

--- kws_streaming/models/transformer_utils.py
+++ kws_streaming/models/transformer_utils.py
@@ -40,11 +40,14 @@ class MultiHeadSelfAttention(tf.keras.layers.Layer):
                 f"embedding dimension = {embed_dim} should be divisible by number of heads = {num_heads}"
             )
         self.projection_dim = embed_dim // num_heads
-        self.query_dense = Dense(embed_dim, kernel_initializer=TruncatedNormal(mean=0., stddev=TRUNC_STD), use_bias=False)
-        self.key_dense = Dense(embed_dim, kernel_initializer=TruncatedNormal(mean=0., stddev=TRUNC_STD), use_bias=False)
-        self.value_dense = Dense(embed_dim, kernel_initializer=TruncatedNormal(mean=0., stddev=TRUNC_STD), use_bias=False)
-        self.combine_heads = Dense(embed_dim, kernel_initializer=TruncatedNormal(mean=0., stddev=TRUNC_STD), bias_initializer=Zeros())
-
+        self.query_dense = Dense(embed_dim, kernel_initializer=TruncatedNormal(
+            mean=0., stddev=TRUNC_STD), use_bias=False)
+        self.key_dense = Dense(embed_dim, kernel_initializer=TruncatedNormal(
+            mean=0., stddev=TRUNC_STD), use_bias=False)
+        self.value_dense = Dense(embed_dim, kernel_initializer=TruncatedNormal(
+            mean=0., stddev=TRUNC_STD), use_bias=False)
+        self.combine_heads = Dense(embed_dim, kernel_initializer=TruncatedNormal(
+            mean=0., stddev=TRUNC_STD), bias_initializer=Zeros())
 
     def attention(self, query, key, value):
         score = tf.matmul(query, key, transpose_b=True)
@@ -54,13 +57,11 @@ class MultiHeadSelfAttention(tf.keras.layers.Layer):
         output = tf.matmul(weights, value)
         return output, weights
 
-
     def separate_heads(self, x, batch_size):
         x = tf.reshape(
             x, (batch_size, -1, self.num_heads, self.projection_dim)
         )
         return tf.transpose(x, perm=[0, 2, 1, 3])
-
 
     def call(self, inputs):
         batch_size = tf.shape(inputs)[0]
@@ -79,16 +80,24 @@ class MultiHeadSelfAttention(tf.keras.layers.Layer):
         output = self.combine_heads(concat_attention)
         return output, weights
 
+
 class TransformerBlock(tf.keras.layers.Layer):
     def __init__(self, embed_dim, num_heads, ff_dim, dropout=0.1, prenorm=False, approximate_gelu=False):
         super(TransformerBlock, self).__init__()
         self.att = MultiHeadSelfAttention(embed_dim, num_heads)
 
+        def mish(inputs):
+            x = tf.nn.softplus(inputs)
+            x = tf.nn.tanh(x)
+            x = tf.multiply(x, inputs)
+            return x
         self.ffn = tf.keras.Sequential(
             [
-                Dense(ff_dim, kernel_initializer=TruncatedNormal(mean=0., stddev=TRUNC_STD), bias_initializer=Zeros()),
-                tfa.layers.GELU(approximate=approximate_gelu),
-                Dense(embed_dim, kernel_initializer=TruncatedNormal(mean=0., stddev=TRUNC_STD), bias_initializer=Zeros()),
+                Dense(ff_dim, kernel_initializer=TruncatedNormal(
+                    mean=0., stddev=TRUNC_STD), bias_initializer=Zeros(), activation=mish),
+                # tfa.layers.GELU(approximate=approximate_gelu),
+                Dense(embed_dim, kernel_initializer=TruncatedNormal(
+                    mean=0., stddev=TRUNC_STD), bias_initializer=Zeros()),
             ]
         )
 
@@ -98,7 +107,6 @@ class TransformerBlock(tf.keras.layers.Layer):
         self.dropout1 = Dropout(dropout)
         self.dropout2 = Dropout(dropout)
         self.prenorm = prenorm
-
 
     def call(self, inputs, training):
         if self.prenorm:
@@ -141,15 +149,18 @@ class KWSTransformer(tf.keras.Model):
         additional_tokens = 2 if distill_token else 1
         self.pos_emb = self.add_weight(
             "pos_emb", shape=(1, num_patches + additional_tokens, d_model), initializer=TruncatedNormal(mean=0., stddev=TRUNC_STD))
-        self.class_emb = self.add_weight("class_emb", shape=(1, 1, d_model), initializer=TruncatedNormal(mean=0., stddev=TRUNC_STD))
-        self.distill_emb = self.add_weight("distill_emb", shape=(1, 1, d_model), initializer=TruncatedNormal(mean=0., stddev=TRUNC_STD)) if distill_token else None
-        self.patch_proj = Dense(d_model, kernel_initializer=TruncatedNormal(mean=0., stddev=TRUNC_STD), bias_initializer=Zeros(), input_shape=(98,40,))
+        self.class_emb = self.add_weight("class_emb", shape=(
+            1, 1, d_model), initializer=TruncatedNormal(mean=0., stddev=TRUNC_STD))
+        self.distill_emb = self.add_weight("distill_emb", shape=(
+            1, 1, d_model), initializer=TruncatedNormal(mean=0., stddev=TRUNC_STD)) if distill_token else None
+        self.patch_proj = Dense(d_model, kernel_initializer=TruncatedNormal(
+            mean=0., stddev=TRUNC_STD), bias_initializer=Zeros(), input_shape=(98, 40,))
 
         self.enc_layers = [
-            TransformerBlock(d_model, num_heads, mlp_dim, dropout, prenorm, approximate_gelu)
+            TransformerBlock(d_model, num_heads, mlp_dim,
+                             dropout, prenorm, approximate_gelu)
             for _ in range(num_layers)
         ]
-
 
     def extract_patches(self, images):
         batch_size = tf.shape(images)[0]
@@ -162,7 +173,6 @@ class KWSTransformer(tf.keras.Model):
         )
         patches = tf.reshape(patches, [batch_size, -1, self.patch_dim])
         return patches
-
 
     def call(self, x, training):
         batch_size = tf.shape(x)[0]
